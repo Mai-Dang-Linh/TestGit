f615e40ac11f83f012aa765af67e9e374ee7f6f3
LINH Mai <tigengalaxy@gmail.com>
2021-07-14 14:30:31 +0700

    create MathFile1

---
+++ MathFile1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int tong(int a, int b)
+{
+    int c = a + b;
+    return c;
+}
+int main()
+{
+    int a =9; int b =10;
+    cout << tong(a,b);
+    return 0;
+}
